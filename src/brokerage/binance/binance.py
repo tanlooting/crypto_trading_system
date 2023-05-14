62e8ad0dc9577ccbb43ad7173db564fa4e751214
Chun Kiat Ong <chunkiat@Chuns-MacBook-Pro.local>
2023-05-15 00:24:19 +0800

    added python-binance get ohlc historical data function

---
+++ src/brokerage/binance/binance.py
@@ -0,0 +1,18 @@
+from src.brokerage.binance.TradeClient import TradeClient
+from src.brokerage.binance.ServiceClient import ServiceClient
+
+
+class Binance:
+    def __init__(self, auth_config: dict, mkt_event_engine, action_event_engine):
+        self.trade_client = TradeClient(
+            auth_config=auth_config,
+            mkt_event_engine=mkt_event_engine,
+            action_event_engine=action_event_engine,
+        )
+        self.service_client = ServiceClient()
+
+    def get_service_client(self):
+        return self.service_client
+
+    def get_trade_client(self):
+        return self.trade_client
